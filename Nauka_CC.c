2d2b719599dafb174cee63c9247a8f63969e2e19
BartoszEff <261398@student.pwr.edu.pl>
2024-10-15 14:07:28 +0200

    Pierwszy

---
+++ Nauka_CC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main()
+{
+printf("Hej co tam u Ciebie");
+
+}
